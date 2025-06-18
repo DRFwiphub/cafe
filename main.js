4c106dbd79e793ba09c1e652a355d2d01c6b4c26
DFlattwoc <d.flatt13@outlook.com>
2025-06-18 11:45:39 +0100

    new update to main

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
-//hello, im here just chillin
+//swiper code start
+//const swiper = 
+new Swiper('.card-wrapper',) {
+
+
+  loop: true,
+  spaceBetween: 30,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true
+  }
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+  breakpoints: {
+    0: {
+        slidesPerView: 1
+    }
+    768: {
+        slidesPerView: 2
+    }
+    1024: {
+        slidesPerView: 3
+    }
+  }
+};
+
+
+//swiper code end
